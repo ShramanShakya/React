295990a34857b4db425d5dc26f31e198cd6076b0
Nightbot <shraman2106@gmail.com>
2025-12-02 20:27:35 +0700

    headerPageUpdating

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Container from './Container'
 import Logo from './Logo'
+import HeaderMenu from './HeaderMenu'
 
 const Header = () => {
   return (
-    <header className="bg-white py-5"> 
-      <Container>
+    <header className="bg-white py-5 border-b border-b-black/20"> 
+      <Container className="flex items-center justify-between">
         <Logo/>
+        <HeaderMenu/>
+      <div>
+        Others
+      </div>
       </Container>
     </header>
   )
