b3e9df589d750940538ff8f86b9fe7956ea17851
Nightbot <shraman2106@gmail.com>
2025-12-05 23:01:10 +0700

    workingOnSidemenu

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
 
-const Logo = ({ className }: {className?:string}) => {
+
+const Logo = ({ className, spanDesignN }: {className?:string, spanDesignN?:string}) => {
   return (
     <Link href={"/"}>
         <h2 className={cn("group text-2xl text-[var(--color-shop_dark_purple)] font-black tracking-wider uppercase hover:text-[var(--color-shop_light_purple)] font-sans",className)}>
             SHOPMA
-            <span className="text-[var(--color-shop_light_purple)] 
-            group-hover:text-[var(--color-shop_dark_purple)] hoverEffect">N</span>
+            <span className={cn("text-[var(--color-shop_light_purple)] group-hover:text-[var(--color-shop_dark_purple)] hoverEffect",spanDesignN)}>N</span>
         </h2>
     </Link>
   )
--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { FC } from 'react'
+import Logo from './Logo'
+import { X } from 'lucide-react'
+
+
 interface SideBarProps{
     isOpen: boolean;
     onClose: ()=> void;
@@ -7,8 +11,15 @@ interface SideBarProps{
 
 const SideMenu : FC<SideBarProps>=({isOpen, onClose}) => {
   return (
-    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 shadow-xl ${isOpen? 'translate-x-0': '-translate-x-full'}hoverEffect`}>
-        SideMenu
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white shadow-xl ${isOpen? 'translate-x-0': '-translate-x-full'}hoverEffect`}>
+        <div className="min-wd-72 max-w-96 bg-black h-screen p-10 border-r border-r-purple">
+            <div className="flex items-center justify-between gap-5">
+                <Logo className="text-white" spanDesignN="group-hover:text-white"/> 
+                <button className="hover:text-[var(--color-shop_light_purple)] absolute top-5 right-5" onClick={onClose}> 
+                    <X/>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
