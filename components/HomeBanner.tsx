d730e98180e10f4aa72f885f9cbbcf05846343c8
ShramanShakya <u6712145@au.edu>
2025-12-14 21:02:14 +0700

    middle

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Title } from './MiddleText';
 
 const HomeBanner = () => {
   return (
-    <div>
-      HomeBanner
+    <div className="py-16 md:py-0 bg-(--color-shop_light_cream) rounded-lg px-10 lg:px-24 flex items-center justify-between">
+<div>
+  <Title>
+    Grab Upto 50% off on <br />
+    Selected Products
+  </Title>
+</div>  
+    <div></div>   
     </div>
   )
 }
