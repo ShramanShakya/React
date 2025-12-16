1634a29f25915717a45af0f444e43e03bbb37f8c
ShramanShakya <u6712145@au.edu>
2025-12-16 18:44:18 +0700

    fix

--- components/MiddleText.tsx
+++ components/MiddleText.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 
  const Title=({children,className}:{children:React.ReactNode,className?:string;})=>{
-    return( <h2 className={cn("text-3xl font-bold text-shop_dark_purple capitalize tracking-wide", className)}>{children}</h2>
+    return( <h2 className={cn("text-3xl font-bold text-(--color-shop_dark_purple) capitalize tracking-wide", className)}>{children}</h2>
  );
 };
 
