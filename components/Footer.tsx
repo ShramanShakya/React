38ff1a619b2ff0e477d91f12e9f311bbabb2daed
ShramanShakya <u6712145@au.edu>
2025-12-18 20:41:17 +0700

    seconddaryfooter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,24 +2,6 @@ import React from 'react'
 import Container from './Container'
 import SecondaryFooter from './SecondaryFooter'
 
-
-interface ContactItemData{
-  title:string;
-  subtitle: string;
-  icon: React.ReactNode;
-}
-const data: ContactItemData[]=[
-  {
-    title:"Vist Us",
-    subtitle:"123 Main St, City, Country",
-    icon:()
-  },
-  {
-    title:"Call Us",
-    subtitle:"0918742671",
-    icon:()
-  }
-]
 const Footer = () => {
   return (
     <footer className="mb-40"> 
--- components/SecondaryFooter.tsx
+++ components/SecondaryFooter.tsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { Clock, Mail, MapPin, Phone } from 'lucide-react';
+
+
+interface ContactItemData{
+  title:string;
+  subtitle: string;
+  icon: React.ReactNode;
+}
+const data: ContactItemData[]=[
+  {
+    title:"Vist Us",
+    subtitle:"123 Main St, City, Country",
+    icon:(
+      <MapPin className="h-6 w-6 text-gray-600 group-hover:text-primary transition-colors"></MapPin>
+    )
+  },
+  {
+    title:"Call Us",
+    subtitle:"0918742671",
+    icon:(
+      <Phone className = "h-6 w-6 text-gray-600 group-hover:text-primary transition-colors"></Phone>
+    )
+  },
+  {
+    title: "Working Hours",
+    subtitle: "Mon - Fri: 9:00 AM - 6:00 PM",
+    icon:(
+      <Clock className = "h-6 w-6 text-gray-600 group-hover:text-primary transition-colors"></Clock>
+    )
+  },
+  {
+    title:"Email Us",
+    subtitle:"Shopman@gmail.com",
+    icon:(
+      <Mail className = "h-6 w-6 text-black-600 group-hover:text-primary transition-colors"></Mail>
+    )
+  }
+]
 
 const secondaryFooter = () => {
   return (
-    <div>
-      SF
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 border-b">
+      {data?.map((item, index)=>(
+        <div key={index} className="flex items-center gap-3 group hover:bg-black-50 p-4 transition-colors">
+          {item?.icon}
+          <div>
+            <h3>{item?.title}</h3>
+            <p>{item?.subtitle}</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
