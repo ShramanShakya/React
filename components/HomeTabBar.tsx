a776bfa5bd104de2540631cd458ba882e8f125d4
ShramanShakya <u6712145@au.edu>
2025-12-26 23:14:24 +0700

    sanity ver change,added loading data

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -2,14 +2,25 @@ import Link from 'next/link'
 import React from 'react'
 import { productType } from './Constants/Data'
 
-const HomeTabBar = () => {
+interface Props{
+    selectedTab: string;
+    onTabSelect: (tab: string) => void;
+}
+
+const HomeTabBar = ({ selectedTab, onTabSelect }: Props) => {
+  console.log(selectedTab);
   return (
     <div className="flex items-center justify-between flex-wrap gap-5">
         <div className="flex items-center gap-3 text-sm font-semibold">
             {productType?.map((item) =>(
-                <button key={item?.value} className={`border border-gray-300
+                <button 
+                onClick={() => onTabSelect(item?.title)}
+                key={item?.title} 
+                className={`border border-gray-300
                     px-4 py-1.5 md:px-6 md:py-2 rounded-full hover:bg-(--color-shop_dark_purple) 
-                    hover:border-(--color-shop_dark_purple) hover:text-white`}>{item?.title}</button>
+                    hover:border-(--color-shop_dark_purple) text-white 
+                    ${selectedTab === item?.title ? "bg-(--color-shop_dark_purple)" : "bg-black"}`}>
+                      {item?.title}</button>
             ))}
         </div>
       <div>Left </div>
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -3,15 +3,47 @@ import { Home } from 'lucide-react';
 import React from 'react'
 import { useState } from 'react';
 import HomeTabBar from './HomeTabBar';
+import { productType } from './Constants/Data';
+import { useEffect } from 'react';
+import { client } from '@/sanity/lib/client';
+import {motion} from "motion/react"
+import { Loader2 } from 'lucide-react';
+
 
 const ProductGrid = () => {
-    const [prodcts, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [selectedTab,setselectedTab] = useState("")
+    const [selectedTab,setselectedTab] = useState(productType[0]?.title||"");
     
+    const query = `*[_type == "product" && variant ==$variant] | order(name desc){
+  ..., "categories": categories[]-> title
+}`;
+const params = { variant: selectedTab.toLowerCase() };
+useEffect(() =>{
+      const fetchData = async() =>{
+        setLoading(true);
+        try{
+          const response = await client.fetch(query,params);
+          setProducts(response);
+        }catch (error){
+          console.error("Error fetching products:", error);
+        }finally{
+          setLoading(false);
+        }
+      }
+      fetchData();
+    },[selectedTab])
   return (
     <div>
-      <HomeTabBar/>
+      <HomeTabBar selectedTab ={selectedTab} onTabSelect = {setselectedTab}/>
+      {!loading? <div className="flex flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
+        <motion.div>
+          <Loader2/>
+          <span>
+            Product is loading...
+          </span>
+        </motion.div>
+      </div> : <>products</>}
     </div>
   )
 }
